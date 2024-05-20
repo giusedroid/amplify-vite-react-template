c06158cb9226f4c42a3a07b07a7c816975487994
gbatt <gbatt@amazon.com>
2024-05-20 23:19:06 +0100

    adding external storage

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,7 +2,14 @@ import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
 
-defineBackend({
+const backend = defineBackend({
   auth,
   data,
 });
+
+backend.addOutput({
+  storage: {
+    bucket_name: 'magiccarpet-manual-tests-dev',
+    aws_region: 'us-west-2'
+  }
+})
